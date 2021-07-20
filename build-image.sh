1dd2720f9152b7434de00546cdd4c27158ca4e42
Ojvar <MOjvar.AmirHossein@Gmail.com>
2021-07-20 10:04:21 +0430

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 # Comment this lien In case of using docker
 alias docker=podman
 
-docker build -f docker/Dockerfile -t sunset5:latest .
-# docker build -f docker/Dockerfile -t sunset5:latest . --build-arg HTTPS=1
+docker build -f docker/Dockerfile -t logger-service:latest .
+# docker build -f docker/Dockerfile -t logger-service:latest . --build-arg HTTPS=1
